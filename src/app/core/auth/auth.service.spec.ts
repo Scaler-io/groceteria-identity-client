d311fa492512d95aa03ae8bb04e3dfb01e1fe51e
sharthak95 <sharthakmallik@gmail.com>
2023-09-30 16:46:48 +0530

    unit test fix

--- src/app/core/auth/auth.service.spec.ts
+++ src/app/core/auth/auth.service.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 import { AuthModule } from 'angular-auth-oidc-client';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [AuthModule.forRoot({})],
+      imports: [AuthModule.forRoot({}), RouterTestingModule],
     });
     service = TestBed.inject(AuthService);
   });
